3faec8c1b08450cc26c06d708ad778729e5f0f17
MonboSoNasty <headmonbo@gmail.com>
2025-09-18 20:32:02 -0400

    Update Addon.java

--- src/main/java/com/MonboSoNasty/meteorchatgptx/Addon.java
+++ src/main/java/com/MonboSoNasty/meteorchatgptx/Addon.java
@@ -1,15 +1,16 @@
-package com.MonboSoNasty.meteorchatgptx.addon;
+package com.MonboSoNasty.meteorchatgptx;
 
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import com.MonboSoNasty.meteorchatgptx.addon.modules.ChatGPTResponder;
+import com.MonboSoNasty.meteorchatgptx.modules.ChatGPTResponder;
 
 public class Addon {
+    // Category for ChatGPT modules in Meteor GUI
     public static final Category CHATGPT_CATEGORY = new Category("ChatGPT", "ChatGPT Modules");
 
     public static void onInitialize() {
+        // Register the ChatGPTResponder module
         Modules.get().add(new ChatGPTResponder());
         System.out.println("[ChatGPTResponder] Module registered successfully!");
     }
 }
-
